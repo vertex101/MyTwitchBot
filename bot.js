885f507853f001ff818ce418aba620670388ed7c
Vertex101 <vertex101@gmx.com>
2019-06-20 14:56:01 -0400

    updating for a lurk bot in a channel

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const tmi = require('tmi.js');
-const Rcon = require('modern-rcon');
 const config = require('./config/cfg.json');
+const fs = require('fs');
 
 let options = {
     options: {
@@ -14,10 +14,22 @@ let options = {
         username: config.twitch.nick,
         password: config.twitch.oauth
     },
-    channels: [ "#vertex101" ]
+    channels: ["#rusty_blitzcrank"]
 };
 
 let client = new tmi.client(options);
 
 // Connect the client to the server..
 client.connect();
+
+client.on("chat", (channel, userstate, message, self) => {
+    // Don't listen to my own messages..
+    if (self) return;
+    // Do your stuff.
+});
+
+function sendCMD() {
+    client.say("#rusty_blitzcrank", "!grab");
+  }
+
+setInterval(sendCMD, 2100000)
