b362aa4769fc58578d5f59f6a94150f24a08eaac
Vertex101 <vertex101@gmx.com>
2019-06-24 02:30:35 -0400

    editing the bot adding in command for a channel

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const tmi = require('tmi.js');
+var request = require('request');
 const config = require('./config/cfg.json');
 const fs = require('fs');
 
@@ -14,7 +15,7 @@ let options = {
         username: config.twitch.nick,
         password: config.twitch.oauth
     },
-    channels: ["#rusty_blitzcrank"]
+    channels: ["#rusty_blitzcrank", "finncapp", "vertex101"]
 };
 
 let client = new tmi.client(options);
@@ -22,14 +23,23 @@ let client = new tmi.client(options);
 // Connect the client to the server..
 client.connect();
 
-client.on('connected', (address, port) => {
-    client.say("#rusty_blitzcrank", "!grab");
-});
-
-client.on("chat", (channel, userstate, message, self) => {
+client.on("chat", (channel, user, message, self) => {
     // Don't listen to my own messages..
     if (self) return;
     // Do your stuff.
+    msg = message.split(" ");
+    if(channel == "#finncapp" || channel == "#vertex101"){
+        if(user.username == channel.replace("#", "") || user.username == "vertex101"){
+            if(msg[0] == "!ex"){
+                request('https://poe.ninja/api/Data/GetCurrencyOverview?league=Legion', function (error, response, body) {
+                    pullData = JSON.parse(body);
+                    setTimeout(function () {
+                        client.say(channel, "1 Exalted Orb is equal to " + pullData.lines[2].chaosEquivalent + " Chaos")
+                    }, 3000); 
+                });
+            }
+        }
+    }
 });
 
 function sendCMD() {
---
+++ startup.bat
@@ -0,0 +1,1 @@
+start "" "%ProgramFiles%\Git\git-bash.exe" -c "node bot.js"
